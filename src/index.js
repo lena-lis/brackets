4885e5ca34b30669682ca431091adb441c8608d0
Elena Lisichenok <111569149+lena-lis@users.noreply.github.com>
2024-08-20 20:22:46 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  const brackets = Object.fromEntries(bracketsConfig);
+
+  for (let i = 0; i < str.length; i++) {
+    if (brackets[str[i]]) {
+      if (str[i] === brackets[str[i]] && stack.includes(str[i])) {
+        stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
+    } else {
+      if (brackets[stack.pop()] !== str[i]) return false;
+    }
+  }
+  
+  return stack.length === 0;
 }
